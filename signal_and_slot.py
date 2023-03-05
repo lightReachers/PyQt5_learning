e218249c7d5ab9c3c43d8862f890a2b6b28e244b
himanshu.solanki <umt157@gmail.com>
2023-03-05 20:16:47 +0800

    add signal slot example

---
+++ signal_and_slot.py
@@ -0,0 +1,27 @@
+from PySide2.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QPushButton
+)
+
+import sys
+
+
+class MainWindow(QMainWindow):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.setWindowTitle("My App")
+        button = QPushButton("Press Me")
+        button.setCheckable(True)
+        button.clicked.connect(self.the_button_was_clicked)
+
+        self.setCentralWidget(button)
+
+    def the_button_was_clicked(self):
+        print("Clicked")
+
+app = QApplication(sys.argv)            
+windows = MainWindow()
+windows.show()
+app.exec_()
